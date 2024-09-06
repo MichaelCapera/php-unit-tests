f647bada792deb14edd57e6ac0ea2c35da134b5c
MAC <michaelcapera@gmail.com>
2024-09-05 20:59:16 -0500

    Start project :rocket:

---
+++ src/Calculator.php
@@ -0,0 +1,10 @@
+<?php
+namespace MyCompany\MyNewProject;
+
+class Calculator
+{
+    public function add($a, $b)
+    {
+        return $a + $b;
+    }
+}
---
+++ tests/CalculatorTest.php
@@ -0,0 +1,13 @@
+<?php
+use MyCompany\MyNewProject\Calculator;
+use PHPUnit\Framework\TestCase;
+
+class CalculatorTest extends TestCase
+{
+    public function testAdd()
+    {
+        $calculator = new Calculator();
+        $result = $calculator->add(2, 3);
+        $this->assertEquals(5, $result);
+    }
+}
